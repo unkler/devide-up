37cef26ea1be1d39157a0cd7b842a9d7ad0013c6
unkler <unkler0830@gmail.com>
2024-10-16 02:43:07 +0900

    router.jsをvue.js2.7.16に対応する記載に変更

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -1,8 +1,5 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import TaskAssignIndex from './pages/TaskAssign/Index.vue'
-import TaskAssignCreate from './pages/TaskAssign/Create.vue'
-import TaskAssignEdit from './pages/TaskAssign/Edit.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
 import EmployeeIndex from './pages/Employee/Index.vue'
 import EmployeeCreate from './pages/Employee/Create.vue'
 import EmployeeEdit from './pages/Employee/Edit.vue'
@@ -12,87 +9,88 @@ import ClientEdit from './pages/Client/Edit.vue'
 import WorkplaceIndex from './pages/Workplace/Index.vue'
 import WorkplaceCreate from './pages/Workplace/Create.vue'
 import WorkplaceEdit from './pages/Workplace/Edit.vue'
-import NotFound from './pages/NotFound.vue'
+import TaskAssignIndex from './pages/TaskAssign/Index.vue'
+import TaskAssignCreate from './pages/TaskAssign/Create.vue'
+import TaskAssignEdit from './pages/TaskAssign/Edit.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/task_assign',
-    name: 'taskAssign',
-    component: TaskAssignIndex,
-    props: true,
-  },
-  {
-    path: '/task_assign/create',
-    name: 'taskAssignCreate',
-    component: TaskAssignCreate,
-  },
-  {
-    path: '/task_assign/edit/:id',
-    name: 'taskAssignEdit',
-    component: TaskAssignEdit,
-    props: true,
-  },
-  {
-    path: '/employee',
-    name: 'employee',
-    component: EmployeeIndex,
-    props: true,
-  },
-  {
-    path: '/employee/create',
-    name: 'employeeCreate',
-    component: EmployeeCreate,
-  },
-  {
-    path: '/employee/edit/:id',
-    name: 'employeeEdit',
-    component: EmployeeEdit,
-    props: true,
-  },
-  {
-    path: '/client',
-    name: 'client',
-    component: ClientIndex,
-    props: true,
-  },
-  {
-    path: '/client/create',
-    name: 'clientCreate',
-    component: ClientCreate,
-  },
-  {
-    path: '/client/edit/:id',
-    name: 'clientEdit',
-    component: ClientEdit,
-    props: true,
-  },
-  {
-    path: '/workplace',
-    name: 'workplace',
-    component: WorkplaceIndex,
-    props: true,
-  },
-  {
-    path: '/workplace/create',
-    name: 'workplaceCreate',
-    component: WorkplaceCreate,
-  },
-  {
-    path: '/workplace/edit/:id',
-    name: 'workplaceEdit',
-    component: WorkplaceEdit,
-    props: true,
-  },
-  { path: '/*', 
-    component: NotFound,
-  },
-]
-
-const router = new VueRouter({
-    mode: 'history',
-    routes
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/employee',
+      name: 'employee',
+      component: EmployeeIndex,
+      props: true,
+    },
+    {
+      path: '/employee/create',
+      name: 'employeeCreate',
+      component: EmployeeCreate,
+      props: true,
+    },
+    {
+      path: '/employee/edit/:id',
+      name: 'employeeEdit',
+      component: EmployeeEdit,
+      props: true,
+    },
+    {
+      path: '/client',
+      name: 'client',
+      component: ClientIndex,
+      props: true,
+    },
+    {
+      path: '/client/create',
+      name: 'clientCreate',
+      component: ClientCreate,
+    },
+    {
+      path: '/client/edit/:id',
+      name: 'clientEdit',
+      component: ClientEdit,
+      props: true,
+    },
+    {
+      path: '/workplace',
+      name: 'workplace',
+      component: WorkplaceIndex,
+      props: true,
+    },
+    {
+      path: '/workplace/create',
+      name: 'workplaceCreate',
+      component: WorkplaceCreate,
+    },
+    {
+      path: '/workplace/edit/:id',
+      name: 'workplaceEdit',
+      component: WorkplaceEdit,
+      props: true,
+    },
+    {
+      path: '/task_assign',
+      name: 'taskAssign',
+      component: TaskAssignIndex,
+      props: true,
+    },
+    {
+      path: '/task_assign/create',
+      name: 'taskAssignCreate',
+      component: TaskAssignCreate,
+    },
+    {
+      path: '/task_assign/edit/:id',
+      name: 'taskAssignEdit',
+      component: TaskAssignEdit,
+      props: true,
+    },
+    {
+      path: '/*',
+      component: () => import('./pages/NotFound.vue'),
+      
+    },
+  ],
 })
-
-export default router
